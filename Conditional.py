5793666946be759ead1ea79a211d460f29e95c7b
shahriarsazid <sazid1190849@gamil.com>
2023-02-18 00:24:27 +0600

    5changes

---
+++ Conditional.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter your course marks:"))
+
+if marks>=80:
+    print("Congratulations!You got A+")
+elif marks>=70:
+    print("You got A")
+elif marks>=60:
+    print("You got A-")
+elif marks>=50:
+    print("You got B")
+elif marks>=40:
+    print("You got C")
+elif marks>=33:
+    print("You got D")
+else:
+    print("You are Failed")
