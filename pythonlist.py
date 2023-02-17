de63ca2b7294fe4c5899980a29b3e0f13a9b9a84
shahriarsazid <sazid1190849@gamil.com>
2023-02-17 23:57:04 +0600

    4changes

---
+++ pythonlist.py
@@ -0,0 +1,12 @@
+fruitlist = ["Banana","Apple","Grap","Orange"]
+add = fruitlist.insert(4,"Cherry")
+ins = fruitlist.append("Pineapple")
+mylist = fruitlist.copy()
+
+print(fruitlist)
+# print(fruitlist.reverse())
+# print(fruitlist.pop())
+# print(ins)
+# print(add)
+# print(fruitlist.sort())
+# print(mylist)
