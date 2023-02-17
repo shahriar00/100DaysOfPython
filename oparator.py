ea9d549ba6d3e80031c16aa66cc99c16f3fe4c92
shahriarsazid <sazid1190849@gamil.com>
2023-02-17 12:33:09 +0600

    second

---
+++ oparator.py
@@ -0,0 +1,23 @@
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+addition = number1+number2
+Subtraction = number1-number2
+Multiplication = number1*number2
+Division = number1/number2
+Modulus = number1%number2
+Exponentiation = number1**number2
+Floor_Division = number1//number2
+
+print(addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+print(Modulus)
+print(Exponentiation)
+print(Floor_Division)
+
+
+
+
+
