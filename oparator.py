8a224997625a5ac197de7fb66c31dde5cd013c20
Sazid <70763173+shahriar00@users.noreply.github.com>
2023-02-18 00:34:39 +0600

    Update oparator.py

--- oparator.py
+++ oparator.py
@@ -1,7 +1,7 @@
 number1 = int(input("Enter first number:"))
 number2 = int(input("Enter second number:"))
 
-addition = number1+number2
+Addition = number1+number2
 Subtraction = number1-number2
 Multiplication = number1*number2
 Division = number1/number2
@@ -9,7 +9,7 @@ Modulus = number1%number2
 Exponentiation = number1**number2
 Floor_Division = number1//number2
 
-print(addition)
+print(Addition)
 print(Subtraction)
 print(Multiplication)
 print(Division)
