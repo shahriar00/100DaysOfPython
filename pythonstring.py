bdc2516e78ca094fda5dec04a73b13144a7b17a0
shahriarsazid <sazid1190849@gamil.com>
2023-02-17 22:29:21 +0600

    thirdchanges

---
+++ pythonstring.py
@@ -0,0 +1,9 @@
+pystring = "Hello This python string "
+string = "Hello world"
+print(pystring)
+print(pystring[2:8])
+print(pystring.split())
+print(pystring.upper())
+print(pystring.lower())
+print(pystring.strip())
+print(pystring+string)
