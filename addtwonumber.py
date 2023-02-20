95813f5f053f866d30816e2af33218ee1f13def9
Sazid <70763173+shahriar00@users.noreply.github.com>
2023-02-20 18:00:36 +0600

    addtwonumber

--- addtwonumber.py
+++ addtwonumber.py
@@ -1,6 +1,5 @@
 first_number = int(input("Enter your First Number:"))
 second_number = int(input("Enter your Second Number:"))
-
 sum = first_number+second_number
 
 print("Sumation of two number:",sum)
