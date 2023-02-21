e24a77d79e4c3465fd81ec2085955b4f520c87c8
Sazid <70763173+shahriar00@users.noreply.github.com>
2023-02-21 11:22:05 +0600

    tuples

--- PythonTuple.py
+++ PythonTuple.py
@@ -1,7 +1,9 @@
 tuple1 = ("apple", "mango", "pineapple", "orange", "banana")
 tuple2 = ("Red", "Yellow", "normal yellow", "orange color", "normal yellow")
 tuple3 = tuple1 + tuple2
+
 # Tuple is order,unchangeable,duplicate values allows
+
 print(len(tuple1))
 print(tuple1)
 print(tuple1[2:3])
