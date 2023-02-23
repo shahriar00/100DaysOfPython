a34b765ddfda43c2b636d1f207cfba813d700461
shahriarsazid <sazid1190849@gamil.com>
2023-02-23 10:12:57 +0600

    set

--- PythonTuple.py
+++ PythonTuple.py
@@ -1,20 +1,24 @@
-tuple1 = ("apple", "mango", "pineapple", "orange", "banana")
-tuple2 = ("Red", "Yellow", "normal yellow", "orange color", "normal yellow")
-tuple3 = tuple1 + tuple2
-# Tuple is order,unchangeable,duplicate values allows
-print(len(tuple1))
-print(tuple1)
-print(tuple1[2:3])
-print(tuple1[-3:])
+# tuple1 = ("apple", "mango", "pineapple", "orange", "banana")
+# tuple2 = ("Red", "Yellow", "normal yellow", "orange color", "normal yellow")
+# tuple3 = tuple1 + tuple2
+# # Tuple is order,unchangeable,duplicate values allows
+# print(len(tuple1))
+# print(tuple1)
+# print(tuple1[2:3])
+# print(tuple1[-3:])
 
-y = list(tuple1)
-y.append("jackfruit")
-# y.remove("jackfruit")
-print(y)
+# y = list(tuple1)
+# y.append("jackfruit")
+# # y.remove("jackfruit")
+# print(y)
+#
+# print(tuple3)
+#
+# # Loop in tuples...
+#
+# for x in tuple1:
+#     print(x)
+first = input()
+last = input()
 
-print(tuple3)
-
-# Loop in tuples...
-
-for x in tuple1:
-    print(x)
+print("Hello "+first+" "+last+"! You just delved into python.")
