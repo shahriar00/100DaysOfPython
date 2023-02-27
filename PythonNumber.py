23a8dc5f86969eca8ce52935ccf98b06f90fa6f8
shahriarsazid <sazid1190849@gamil.com>
2023-02-28 00:24:26 +0600

    number

---
+++ PythonNumber.py
@@ -0,0 +1,20 @@
+# There are three numeric types in Python:
+# int
+# float
+# complex
+
+i = 1
+j = 2.442
+z = 1j
+
+print(type(i))
+print(type(j))
+print(type(z))
+
+interger = int(j)
+print(interger)
+compl = complex(i)
+print(compl)
+
+import random
+print(random.randrange(1,5))
