388728455488d0d629eeae9276417697bf442584
shahriarsazid <sazid1190849@gamil.com>
2023-03-01 13:35:36 +0600

    numpy

---
+++ NumPy.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+n1 = np.array([[12,33,55],[3,5,7]])
+n2 = np.array([[2,2,6],[7,7,4]])
+# print(n1.shape)
+
+# print(np.vstack(n1))
+# print(np.hstack(n2))
+# print(np.column_stack(n1))
+
+# random_number = np.random.randint(100,200,10)
+# print(random_number)
+
+num1 = [2,24,65,7,8]
+num2 = [1,2,3,4,5]
+
+# print(np.intersect1d(num1,num2))
+# print(np.sum([num1,num2]))
+# print(np.sum([num1]))
+
+# n1 = np.array([1,2,43])
+# n1 = n1+1
+# n2 = n1*2
+# n3 = n1-2
+# n4 = n1/2
+# print(n1)
+# print(n2)
+
+# meanvalue = np.mean(n1)
+# print(meanvalue)
+# stand = np.std(n2)
+# print(stand)
+
+number = np.array([[1,2,3,4],[6,7,8,9],[11,22,44,66]])
+number1 = np.array([[1,65,6,7],[2,3,4,5],[7,8,9,44]])
+# print(number)
+# print(number[1])
+# print("Transpose Matrix:\n")
+# print(number.transpose())
+# print(number.dot(number1))
+print(np.arange(10,20))
