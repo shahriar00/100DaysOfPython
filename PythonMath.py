7b685534954d3abd965eec94b98ea7dd353ded5f
shahriarsazid <sazid1190849@gamil.com>
2023-03-01 09:46:43 +0600

    number

---
+++ PythonMath.py
@@ -0,0 +1,21 @@
+import math
+
+mini_num = min(5,2,66)
+max_num = max(33,11,6)
+print(mini_num)
+print(max_num)
+
+absolate = abs(-5.6)
+print(absolate)
+
+square = math.sqrt(3)
+power = pow(3,5)
+print(square)
+
+celing1 = math.ceil(3.5)
+celing2 = math.floor(8.2)
+pinum = math.pi
+print(celing1)
+print(celing2)
+print(pinum)
+
