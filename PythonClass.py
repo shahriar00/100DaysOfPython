f3101cb0c5ec65c8c847e39aba64fcdec27e4a86
shahriarsazid <sazid1190849@gamil.com>
2023-03-07 23:01:44 +0600

    class

---
+++ PythonClass.py
@@ -0,0 +1,12 @@
+class myclass:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+       return f"{self.name,self.age}"
+
+p = myclass("John",33)
+
+print(p)
+# print(p.name)
+# print(p.age)
