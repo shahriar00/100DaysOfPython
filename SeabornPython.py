bd42f00ea12fe1a2da5ec8b4c6358810a6b0bdce
shahriarsazid <sazid1190849@gamil.com>
2023-03-16 12:33:47 +0600

    slicing

--- SeabornPython.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-# tips = sns.load_dataset('tips')
-# print(tips.head())
-
-sns.relplot(data='tips',x='total_bill',y='tip',col='time',hue='smoker',style='smoker',size='size')
---
+++ pythonslicing.py
@@ -0,0 +1,16 @@
+message = "Hello world"
+# message[start:stop:step]
+print(message[3:5])
+print(message[2:6:2])
+print(message[:5])
+print(message[:-1])
+print(message[-1:])
+print(message[-3:])
+print(message[::])
+
+data = "Code yug"
+print(data[-1:-4:-1])
+print(data[0:0])
+print(data[-1:-8:-1])
+print(data[-1::])
+print(data[-5:])
