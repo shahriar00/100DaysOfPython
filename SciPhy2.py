6238137ce22141441d14a1c2bca73c2e3ddfb890
Sazid <70763173+shahriar00@users.noreply.github.com>
2023-04-06 00:03:36 +0600

    scipy

--- SciPhy2.py
+++ SciPhy2.py
@@ -2,7 +2,6 @@ import  numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 
-
 arr = np.array([[3,0,0],[0,1,1]])
 mat = csr_matrix(arr)
 mat.eliminate_zeros()
